0f0aac19b86e3092af3ad8f33bf01248009431ae
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2021-09-30 22:34:07 +0200

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const sizeRangeChange = document.querySelector('#font-size-control');
-
-
-const textSizeChanger = function (event) {
-    document.getElementById("text").style.fontSize = `${Number(event.currentTarget.value)}px`;
-};
-
-sizeRangeChange.addEventListener('change', textSizeChanger);
-
